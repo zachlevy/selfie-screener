02c6a2efb87af477da67c1c5e1047b32e36cecf6
Zachary Levy <zacharyalevy@gmail.com>
2014-11-26 22:22:20 -0500

    added cvs import, better front-end

--- seapp/utils.py
+++ seapp/utils.py
@@ -1,8 +1,29 @@
 from seapp.models import *
 from django.db.models import Count
 from django.conf import settings
+import csv
 
-def import_users():
+def import_users(file_location):
 	print "import_users"
-
-
+	with open(file_location, 'rU') as csvfile:
+		#selfies = csv.reader(csvfile, delimiter=',', quotechar='"')
+		selfies = csv.reader(csvfile)
+		for row in selfies:
+			ig_id = row[1]
+			url = row[0]
+			try:
+				user = User.objects.get(ig_id=ig_id)
+			except:
+				user = User(
+					ig_id = ig_id
+					)
+				user.save()
+				user = User.objects.get(ig_id=ig_id)
+			try:
+				photo = Photo(
+					user = user,
+					url = url
+					)
+				photo.save()
+			except Exception, e:
+				print e
--- seapp/views.py
+++ seapp/views.py
@@ -9,8 +9,13 @@ from django.db.models import Count
 from django.conf import settings
 import urllib
 from seapp.utils import *
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
+
+def test(request):
+	import_users("selfies_import_test.csv")
+	return HttpResponseRedirect(reverse('photos'))
 
 def new_user(request, ig_id):
 	try:
@@ -25,10 +30,7 @@ def new_user(request, ig_id):
 
 def new_photo(request, ig_id, url):
 	try:
-		print ig_id
-		print url
 		user = User.objects.get(ig_id=ig_id)
-		print user
 		photo = Photo(
 			user = user,
 			url = "https://i.imgur.com/9Zx0eij.jpg",
@@ -40,10 +42,15 @@ def new_photo(request, ig_id, url):
 
 def photos(request):
 	photos = Photo.objects.all()
-	print photos
+	paginator = Paginator(photos, 32)
+	page = request.GET.get('page')
+	try:
+		photos_paginated = paginator.page(page)
+	except PageNotAnInteger:
+		return HttpResponseRedirect('/photos/?page=1')
 	return render(request, 'html/photos.html', {
-        'photos' : photos,
-    })
+		'photos' : photos_paginated,
+	})
 
 def users(request):
 	users = User.objects.all()
@@ -54,15 +61,31 @@ def users(request):
 def user(request, ig_id):
 	user = User.objects.get(ig_id=ig_id)
 	user.photos = Photo.objects.filter(user=user)
+	try:
+		next_user = User.objects.get(pk=(user.id + 1))
+	except:
+		next_user = ""
+	try:
+		prev_user = User.objects.get(pk=(user.id - 1))
+	except:
+		prev_user = ""
+
 	return render(request, 'html/user.html', {
         'user' : user,
+        'next_user' : next_user,
+        'prev_user' : prev_user,
     })
 
 def remove_photo(request, photo_id):
-	photo = Photo.objects.get(pk=photo_id)
-	user = photo.user
-	photo.remove()
-	return HttpResponseRedirect(reverse('user', kawrgs={'ig_id':user.ig_id}))
+	try:
+		photo = Photo.objects.get(pk=photo_id)
+		user = photo.user
+		photo.delete()
+	except Exception, e:
+		print e
+		return HttpResponseRedirect(reverse('photos'))
+
+	return HttpResponseRedirect(reverse('user', kwargs={'ig_id':user.ig_id}))
 
 def remove_user(request, ig_id):
 	user = User.objects.filter(ig_id=ig_id).first()
